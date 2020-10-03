13a46d242e752747ffc840a1c387bcd04f163b37
xuwei <email.xuwei@gmail.com>
2020-10-03 23:57:00 +1000

    updated config for test

--- src/test/java/co/zip/candidate/UserApiApplicationTests.java
+++ src/test/java/co/zip/candidate/UserApiApplicationTests.java
@@ -1,17 +1,16 @@
 package co.zip.candidate;
 
-import co.zip.candidate.userapi.UserApiApplication;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = UserApiApplication.class)
+@SpringBootTest
 public class UserApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
-	}
 
+	}
 }
